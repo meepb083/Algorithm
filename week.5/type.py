68c555aea91aebe58a3e6614781f30598a114da5
meepb083 <vivobp36@gmail.com>
2025-12-09 11:55:25 +0700

    Add files via upload

---
+++ week.5/type.py
@@ -0,0 +1,21 @@
+FullName = "Phatsanaphon Khanphu"
+Stdudent = 684245010
+Gpax = 3.25
+Age = 19
+
+if type(FullName) == str:
+  print("Phatsanaphon Khanphu is Str")
+else:
+  print("Phatsanaphon Khanphu is not Str")
+if type(Stdudent) == complex:
+  print("684245010 is complex")
+else:
+  print("684245010 is not complex")
+if type(Gpax) == float:
+  print("3.25 is float")
+else:
+  print("3.25 is not float")
+if type(Age) == int:
+  print("19 is int")
+else:
+  print("19 is not int")
